98231477f53ea6ecdbb4a9cd27339321d2fdc326
raj0212 <49764562+raj0212@users.noreply.github.com>
2019-04-19 00:52:28 +0530

    Add files via upload

---
+++ calci.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	int x,y,sum ,product , difference, division;
+	
+	printf("x\n y");
+	scanf("%d %d",&x,&y);
+	sum=x+y;
+
+	printf("%d",sum);
+	product=x*y;
+printf("\t %d",product);
+	
+	return 0;
+}
